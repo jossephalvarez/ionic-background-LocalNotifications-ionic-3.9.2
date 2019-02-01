bf6f8d57220ecb61f580d0fd41f662be5c37693b
jalvarez <jalvarez@clever-global.com>
2019-02-01 12:48:52 +0100

    BackgroundGeolocation installed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {
+  BackgroundGeolocation,
+  BackgroundGeolocationConfig,
+  BackgroundGeolocationResponse
+} from "@ionic-native/background-geolocation/ngx";
+import {LocalNotifications} from "@ionic-native/local-notifications/ngx";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  config: BackgroundGeolocationConfig = {
+    desiredAccuracy: 10,
+    stationaryRadius: 20,
+    distanceFilter: 30,
+    debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+    stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+  };
 
+  constructor(public navCtrl: NavController,
+              private backgroundGeolocation: BackgroundGeolocation,
+              private localNotifications: LocalNotifications) {
+
+    this.backgroundGeolocation.configure(this.config)
+      .then((location: BackgroundGeolocationResponse) => {
+        console.log(location);
+        this.showNotification(location)
+        // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+        // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+        // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+        this.backgroundGeolocation.finish(); // FOR IOS ONLY
+      });
+  }
+  startBackgroundGeolocation() {
+    // start recording location
+    this.backgroundGeolocation.start();
   }
 
+  stopBackgroundGeolocation() {
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    this.backgroundGeolocation.stop();
+  }
+
+  showNotification(data){
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: JSON.stringify(data),
+      sound: 'file://sound.mp3',
+      data: { secret: "key" }
+    });
+  }
 }
